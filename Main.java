c4b41784b4b86b39b23caf7c38f6e722a6fcdf7d
lohi P <lohis1317@gmail.com>
2021-02-01 02:12:33 +0000

    Added Program

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Ratna!");
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input your first name: ");
+    String fname = input.next();
+    System.out.print("Input your last name: ");
+    String lname = input.next();
+    System.out.println();
+    System.out.println("Hello \n"+fname+" "+lname);
     }
 }
